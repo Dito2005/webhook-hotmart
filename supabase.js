da2f2d006a5b991738bf2447d2948a8baa2e7aa5
Dito2005 <osmargs2005@gmail.com>
2026-03-19 22:21:17 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,1 @@
-const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://ncpmuvbbxzjctxifvkad.supabase.co';
-const supabaseKey = 'sb_publishable_zIknWNVWxOMpW5MSjjLmIw_uB2upjN7';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = supabase;
